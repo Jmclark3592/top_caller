bf553016f2c30c6ec51baf8b371070e75f33f831
Jmclark3592 <jmclark3592@gmail.com>
2023-07-15 11:42:05 -0500

    tracker now using discord webhook

--- whale_tracker/tracker.py
+++ whale_tracker/tracker.py
@@ -3,26 +3,27 @@
 # my address: 0x15378FA7495bac16040877F7E17c6547481687F6
 
 import requests
-import discord
+import json
+from dotenv import load_dotenv
 
-# import json
-# import smtplib
-# from email.message import EmailMessage
+# import smtplib?
+# from email.message import EmailMessage?
 import time
 import os
 
 # connect to API
 url = "https://api.etherscan.io/api"
 address = "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
-API_KEY = os.getenv("API_KEY")
-
+load_dotenv()
+ETHERSCAN_TOKEN = os.getenv("ETHERSCAN_TOKEN")
+DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
 params = {
     "module": "account",
     "action": "balance",
     "address": address,
     "tag": "latest",
-    "apikey": API_KEY,
+    "apikey": ETHERSCAN_TOKEN,
 }
 
 prev_bal = None
@@ -36,10 +37,15 @@ while True:
     current_bal = float(data["result"]) / 10**18
     print(f"Current Balance: {current_bal} ETH")
 
-    if prev_bal is not None and prev_bal - current_bal >= 10000:
-        print("Sending alert - will replace with actual alert")
-        # configure to send a text or email
+    if prev_bal is not None and prev_bal - current_bal >= 4900:
+        print("Sending alert to discord")
+        alert_content = f"The balance has decreased by over 4900 ETH! New balance: {current_bal} ETH"
+        requests.post(
+            DISCORD_WEBHOOK,
+            data=json.dumps({"content": alert_content}),
+            headers={"Content-Type": "application/json"},
+        )
 
     prev_bal = current_bal
 
-    time.sleep(5)  # polling API every 60s
+    time.sleep(5)  # polling API every 60s (set to 5s during testing)
